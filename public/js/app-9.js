de5ddb05420b69c3754de16a990bd89cefa84117
unknown <dig-dug>
2021-08-30 11:46:16 +0200

    comitting1

--- public/js/app-9.js
+++ public/js/app-9.js
@@ -7,25 +7,34 @@ class ProductList extends React.Component {
     this.state = {
       products: [],
     };
-
+    //manual binding inside constructor
+   
     this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
-
+/*
+"map" traverses products array. map returns a new array without
+modifiying original
+ */
   // Inside `ProductList`
   handleProductUpVote(productId) {
     const nextProducts = this.state.products.map((product) => {
+  //if id matches, create new object
       if (product.id === productId) {
+  //"asign()" for avoiding mutating objects, and create a clone
         return Object.assign({}, product, {
+  
           votes: product.votes + 1,
         });
       } else {
+  // return unmodified
         return product;
       }
     });
+  //set state to update
     this.setState({
       products: nextProducts,
     });
@@ -62,7 +71,9 @@ class Product extends React.Component {
 
     this.handleUpVote = this.handleUpVote.bind(this);
   }
-
+   //manual binding inside constructor
+ //handling this function in app-complete with help of babel´s:
+//"transform-class-properties plugin as arrow function"
   handleUpVote() {
     this.props.onVote(this.props.id);
   }
